492f09f6f672a948f9899f5281cd15154bd1c2b2
David van Buiten <david.vanbuiten@gmail.com>
2021-06-28 14:13:08 +0200

    Refactor of email rules/actions + added EmailService + moved tests to the email app and extended the tests

---
+++ api/app/signals/apps/email_integrations/tests/test_tasks.py
@@ -0,0 +1,20 @@
+# SPDX-License-Identifier: MPL-2.0
+# Copyright (C) 2021 Gemeente Amsterdam
+from unittest import mock
+
+from django.test import TestCase
+
+from signals.apps.email_integrations import tasks
+from signals.apps.signals import workflow
+from signals.apps.signals.factories import SignalFactory, StatusFactory
+
+
+class TestTasks(TestCase):
+    @mock.patch('signals.apps.email_integrations.tasks.MailService.mail', autospec=True)
+    def test_send_mail_reporter_created(self, mocked_mail):
+        signal = SignalFactory.create()
+        signal.status = StatusFactory(_signal=signal, state=workflow.BEHANDELING)
+        signal.save()
+
+        tasks.send_mail_reporter(pk=signal.pk)
+        mocked_mail.assert_called_once_with(signal=signal.pk)
